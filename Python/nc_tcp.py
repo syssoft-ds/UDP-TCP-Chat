00391bb3a111014b30607160fac274edd328c7e8
Roman Schander <scharoma@gmail.com>
2024-06-03 19:35:15 +0200

    Rechnernetze Übungsblatt 3

--- Python/nc_tcp.py
+++ Python/nc_tcp.py
@@ -1,6 +1,17 @@
 import socket
 import sys
 import threading
+import getpass
+import regex as re
+
+#Server 
+userlist = {} #list of users with their ip and port
+CMDREGEX = re.compile(r'^[A-Z_]{3,}=')
+
+#Client
+USERNAME = None
+NETINFO = None
+
 
 def server(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_sock:
@@ -8,27 +19,91 @@ def server(port):
         s_sock.listen()
         while True:
             c_sock, c_address = s_sock.accept()
+            port = c_sock.getsockname()[1]
+            print(f'Client {c_address} connected on port {port}')
             t = threading.Thread(target=serveClient,args=(c_sock,c_address))
             t.start()
 
-def serveClient(c_sock,c_address):
+def serveClient(c_sock, c_address):
     with c_sock:
         while True:
             line = c_sock.recv(1024).decode().rstrip()
-            print(f'Message <{repr(line)}> received from client {c_address}')
+            if not line:
+                print(f'Client {c_address} disconnected')
+                #remove user from userlist
+                for user in list(userlist.keys()):
+                    if userlist[user][0] == c_address[0] and userlist[user][1] == c_address[1]:
+                        userlist.pop(user)
+                break
+
+            #Process incoming server requests
+            if CMDREGEX.match(line):
+                cmd = line.split("=")
+                if cmd[0] == 'ADDUSR':
+                    #ADDUSR=<username>;<ip>;<port> -> Adds user to userlist of server
+                    user = cmd[1].split(";")
+                    userlist[user[0]] = (user[1],int(user[2]), c_sock)
+                elif cmd[0] == 'LIST':
+                    #LIST= -> Sends list of users to requesting client
+                    users = [user for user in userlist]
+                    c_sock.sendall(str(users).encode())
+                elif cmd[0] == 'MSG':
+                    #MSG=<sender>;<reciever>;<msg> -> Sends message to reciever
+                    sender, reciever = cmd[1].split(";")[0], cmd[1].split(";")[1]
+                    msg = cmd[1].split(";")[2]
+                    
+                    if reciever in userlist:
+                        try:
+                            reciever_sock = userlist[reciever][2]
+                            reciever_sock.sendall(f'{sender}: {msg}'.encode())
+                        except Exception:
+                            print(f'Error sending message to {reciever}\n{Exception}')
+                    print(f'Message <{repr(msg)}> received from client {sender}, sending to {reciever}')         
+            else:
+                print(f'Unknown interaction <{repr(line)}> received from client {c_address}')
             if line.lower() == 'stop':
                 break
 
 def client(host,port):
+    global USERNAME, NETINFO
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c_sock:
         c_sock.connect((host,port))
-        for line in sys.stdin:
-            line = line.rstrip()
-            c_sock.sendall(line.encode())
-            if line.lower() == 'stop':
-                break
+        NETINFO = (c_sock.getsockname()[0],c_sock.getsockname()[1])
+
+        connectmsg = f'ADDUSR={USERNAME};{NETINFO[0]};{NETINFO[1]}'
+        c_sock.sendall(connectmsg.encode())
+        print(f'Hello {USERNAME}! You are connected to {host}:{port}')
+
+        #start new thread to accept incoming messages
+        threading.Thread(target=display_msg, args=(c_sock,)).start()
+        #client interface
+        while True:
+            try:
+                cmd = input("< ").split(" ")
+                if cmd[0] == 'send':
+                    if len(cmd) < 3:
+                        print("Usage: send <name> <message>")
+                        continue
+                    msg = ' '.join(cmd[2:])
+                    c_sock.sendall(f'MSG={USERNAME};{cmd[1]};{msg}'.encode())
+                elif cmd[0] == 'list':
+                    c_sock.sendall('LIST='.encode())
+                elif cmd[0] == 'help':
+                    print("Available commands: list, send, help, exit")
+                elif cmd[0] == 'exit':
+                    sys.exit()
+                else:
+                    print("Invalid command")
+            except Exception:
+                print("Something went wrong")
+
+def display_msg(c_sock):
+    while True:
+        msg = c_sock.recv(1024).decode()
+        print(f'{msg}')
 
 def main():
+    global USERNAME
     if len(sys.argv) != 3:
         name = sys.argv[0]
         print(f"Usage: \"{name} -l <port>\" or \"{name} <ip> <port>\"")
@@ -37,6 +112,8 @@ def main():
     if sys.argv[1].lower() == '-l':
         server(port)
     else:
+        USERNAME = input("Enter your username: ").replace(" ", "_") or getpass.getuser()
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client(sys.argv[1],port)
 
 if __name__ == '__main__':
--- Python/nc_udp.py
+++ Python/nc_udp.py
@@ -1,5 +1,15 @@
 import socket
 import sys
+import getpass
+import threading
+import regex as re
+
+#Client information
+contacts = {}
+USERNAME = None
+NETINFO = None
+CMDREGEX = re.compile(r'^[A-Z_]{3,}=')
+
 
 def receiveLines(port):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_sock:
@@ -7,28 +17,80 @@ def receiveLines(port):
         while True:
             line, c_address = s_sock.recvfrom(4096)
             line = line.decode().rstrip()
-            print(f'Message <{repr(line)}> received from client {c_address}')
+            
+            #Process incoming requests
+            if CMDREGEX.match(line):
+                cmd = line.split("=")
+                if cmd[0] == 'FRIEND_ADD':
+                    #FRIEND_ADD=<username>;<ip>;<port> -> Adds user to contacts
+                    contact = cmd[1].split(";")
+                    contacts[contact[0]] = (contact[1],int(contact[2]))     #name -> (host,port)
+                    print(f'User {contact[0]} added you as a friend')
+                elif cmd[0] == 'MSG_RECIEVE':
+                    #MSG_RECIEVE=<sender>;<msg> -> Recieve message from a sender
+                    msg = cmd[1].split(";")
+                    print(f'> Message <{repr(msg[1])}> received from client {msg[0]}')    
+            else:
+                print(f'Unknown interaction: <{repr(line)}> received from client {c_address}')
             if line.lower() == 'stop':
                 break
 
-def sendLines(host,port):
+#Added aditional parameter msg to be called from chat_interface
+def sendLines(host, port, msg: str=None):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as c_sock:
-        for line in sys.stdin:
-            line = line.rstrip()
-            c_sock.sendto(line.encode(),(host,port))
-            if line.lower() == 'stop':
-                break
+        if not msg:
+            for line in sys.stdin:
+                line = line.rstrip()
+                c_sock.sendto(line.encode(),(host,port))
+                if line.lower() == 'stop':
+                    break
+        else:
+            c_sock.sendto(msg.encode(),(host,port))
+
+def chat_interface():
+    print(f'Welcome {USERNAME}!')
+    while True:
+        try:
+            cmd = input("< ").split(" ")
+            if cmd[0] == 'add':
+                if len(cmd) < 3:
+                    print("Usage: add <name> <host> <port>")
+                    continue
+                sendLines(cmd[1],int(cmd[2]),f'FRIEND_ADD={USERNAME};{NETINFO[0]};{NETINFO[1]}' )
+            elif cmd[0] == 'send':
+                if len(cmd) < 3:
+                    print("Usage: send <name> <message>")
+                    continue
+                msg = ' '.join(cmd[2:])
+                sendLines(contacts[cmd[1]][0],contacts[cmd[1]][1],f'MSG_RECIEVE={USERNAME};{msg}')
+            elif cmd[0] == 'contacts':
+                print(contacts)
+            elif  cmd[0] == 'whoami':
+                print(f'{USERNAME}: {NETINFO[0]}:{NETINFO[1]}')
+            elif cmd[0] == 'help':
+                print("Available commands: add, send, contacts, whoami, help, exit")
+            elif cmd[0] == 'exit':
+                sys.exit()
+            else:
+                print("Invalid command")
+        except Exception:
+            print("Something went wrong")
 
 def main():
-    if len(sys.argv) != 3:
+    global USERNAME, NETINFO
+    if len(sys.argv) != 2:
         name = sys.argv[0]
-        print(f"Usage: \"{name} -l <port>\" or \"{name} <ip> <port>\"")
+        print(f"Usage: \"{name} <port>\"")
         sys.exit()
-    port = int(sys.argv[2])
-    if sys.argv[1].lower() == '-l':
-        receiveLines(port)
-    else:
-        sendLines(sys.argv[1],port)
+    port = int(sys.argv[1])
+    USERNAME = input("Enter your username: ").replace(" ", "_") or getpass.getuser()
+    threading.Thread(target=receiveLines, args=(port,)).start()
+
+    # Get the host name and port number
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80))
+        NETINFO = (s.getsockname()[0],port)
+    chat_interface()
 
 if __name__ == '__main__':
     main()
