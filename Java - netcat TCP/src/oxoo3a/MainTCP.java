5ad7ddecc8f4064c2fed0d8654750f65dd41971a
Biyan <biyanak03@gmail.com>
2024-06-05 23:39:53 +0200

    versuch übung 3

--- Java - netcat TCP/src/oxoo3a/MainTCP.java
+++ Java - netcat TCP/src/oxoo3a/MainTCP.java
@@ -8,7 +8,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class MainTCP {
 
     private static void fatal ( String comment ) {
         System.out.println(comment);
